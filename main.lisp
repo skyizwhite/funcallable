0ee4faaa7387d6340f40a9b324f0a62163a7e390
paku <paku@skyizwhite.dev>
2022-10-18 20:41:33 +0900

    Define system

--- main.lisp
+++ main.lisp
@@ -1,17 +1,19 @@
-(ql:quickload :closer-mop)
-
-(defpackage :funcallable
+(uiop:define-package :funcallable
+  (:nicknames :funcalable/main)
   (:use :cl)
+  (:import-from :closer-mop
+                :funcallable-standard-class
+                :set-funcallable-instance-function)
   (:export :def-funcallable-class
            :def-funcall))
 (in-package :funcallable)
 
 (defmacro def-funcallable-class (&body body)
-  `(defclass ,@body (:metaclass closer-mop:funcallable-standard-class)))
+  `(defclass ,@body (:metaclass funcallable-standard-class)))
 
 (defmacro def-funcall (cls vars &body body)
   (let ((self (intern (symbol-name 'self))))
     `(defmethod initialize-instance :after ((,self ,cls) &key)
-       (closer-mop:set-funcallable-instance-function
+       (set-funcallable-instance-function
         ,self
         (lambda ,vars ,@body)))))
